a93fa23cc418fb407e9c07c8bfb87b69c5a3a076
Zack Buhman <zack@buhman.org>
2026-03-23 02:50:25 -0500

    fix linux builds

--- include/declarations.h
+++ include/declarations.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#ifdef _WIN32
 #ifdef _MSC_VER
 #define EXPORT __declspec(dllexport)
 #define DECL __cdecl
 #else
 #define EXPORT __attribute__((dllexport))
 #define DECL __attribute__((__cdecl__))
+#endif
+#else
+#define EXPORT
+#define DECL
 #endif
